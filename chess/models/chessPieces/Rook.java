bcb51fb32b50b480395c7ca254db95cd72e67c8e
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2025-03-12 15:02:59 +0530

    Validations : Rook Completedgit add .

--- chess/models/chessPieces/Rook.java
+++ chess/models/chessPieces/Rook.java
@@ -14,7 +14,56 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean validateMove(Move move,Board board) {
+//        TBI: Castling
+
+        int toRow  = move.getToCell().getRow();
+        int toCol = move.getToCell().getCol();
+        int curRow = move.getFromCell().getRow();
+        int curCol = move.getFromCell().getCol();
+
+//        move in row/col only no diagonal / random loc
+        if(toRow!=curRow || toCol!=curCol){
+            return false;
+        }
+        //cannot cross over any one
+        if(toRow==curRow){
+            int start = curCol;
+            int end = toCol;
+//            for loop in one dir
+            if(start>end) {
+                start = toCol;
+                end = curCol;
+            }
+            for(int j = start;j<end;j++){
+                if(isValid(toRow,j)) {
+                    if (board.getBoard().get(toRow).get(j).getCellStatus().equals(CellStatus.FILLED)) {
+                        return false;
+                    }
+                }
+            }
+        }else{
+            int start = curRow;
+            int end = toRow;
+//            for loop in one dir
+            if(start>end) {
+                start = toRow;
+                end = curRow;
+            }
+            for(int i = start;i<end;i++){
+                if(isValid(i,toCol)) {
+                    if (board.getBoard().get(i).get(toCol).getCellStatus().equals(CellStatus.FILLED)) {
+                        return false;
+                    }
+                }
+            }
+        }
         return true;
+    }
+    public boolean isValid(int row,int col){
+        if(row>=0 && row<8 && col>=0 && col<8){
+            return true;
+        }
+        return false;
     }
 
     @Override
